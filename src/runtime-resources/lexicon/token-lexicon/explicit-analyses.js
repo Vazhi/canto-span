f9017f0e8aa596992578fed3871e524cfd63b09c
Vazhi <terazal@gmail.com>
2026-08-14 13:13:26 +0800

    Register native-reviewed lexical correction analyses

--- src/runtime-resources/lexicon/token-lexicon/explicit-analyses.js
+++ src/runtime-resources/lexicon/token-lexicon/explicit-analyses.js
@@ -2,6 +2,7 @@
 
 const { EXPLICIT_ANALYSES: REVIEWED_R251_500_ANALYSES } = require("./cifu-r251-500-reviewed");
 const { EXPLICIT_ANALYSES: REVIEWED_R1_250_ANALYSES } = require("./cifu-r1-250-reviewed");
+const { EXPLICIT_ANALYSES: NATIVE_REVIEW_CORRECTIONS } = require("./native-review-corrections");
 
 // Explicitly polyfunctional lexical analyses represented by bounded contextual
 // runtime behavior or finalized reviewed lexical adjudication. This registry
@@ -53,4 +54,5 @@ module.exports = Object.freeze({
   ...contextualAnalyses,
   ...REVIEWED_R251_500_ANALYSES,
   ...REVIEWED_R1_250_ANALYSES,
+  ...NATIVE_REVIEW_CORRECTIONS,
 });
