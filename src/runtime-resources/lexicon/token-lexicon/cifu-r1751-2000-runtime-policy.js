f4364fe318d1a9bc9fecd263ea9fde75fead95f4
Vazhi <terazal@gmail.com>
2026-08-16 21:24:30 +0800

    Remove source-specific Mandarin exclusion policy

--- src/runtime-resources/lexicon/token-lexicon/cifu-r1751-2000-runtime-policy.js
+++ src/runtime-resources/lexicon/token-lexicon/cifu-r1751-2000-runtime-policy.js
@@ -1,6 +1,17 @@
 "use strict";
 
 const reviewed = require("./cifu-r1751-2000-reviewed");
-const MANDARIN_ONLY_SURFACES = new Set();
-function applyRuntimePolicy(entries) { return reviewed.applyReviewedEntries(entries).filter(([surface]) => !MANDARIN_ONLY_SURFACES.has(surface)); }
-module.exports = Object.freeze({ SOURCE: reviewed.SOURCE, PROMOTIONS: reviewed.PROMOTIONS, RESEARCH_REQUIRED_SURFACES: reviewed.RESEARCH_REQUIRED_SURFACES, BLOCKED_ATOMIC_SURFACES: reviewed.BLOCKED_ATOMIC_SURFACES, CANDIDATE_ONLY_SURFACES: reviewed.CANDIDATE_ONLY_SURFACES, DEFAULT_READING_OVERRIDES: reviewed.DEFAULT_READING_OVERRIDES, MANDARIN_ONLY_SURFACES, applyRuntimePolicy });
+
+function applyRuntimePolicy(entries) {
+  return reviewed.applyReviewedEntries(entries);
+}
+
+module.exports = Object.freeze({
+  SOURCE: reviewed.SOURCE,
+  PROMOTIONS: reviewed.PROMOTIONS,
+  RESEARCH_REQUIRED_SURFACES: reviewed.RESEARCH_REQUIRED_SURFACES,
+  BLOCKED_ATOMIC_SURFACES: reviewed.BLOCKED_ATOMIC_SURFACES,
+  CANDIDATE_ONLY_SURFACES: reviewed.CANDIDATE_ONLY_SURFACES,
+  DEFAULT_READING_OVERRIDES: reviewed.DEFAULT_READING_OVERRIDES,
+  applyRuntimePolicy,
+});
